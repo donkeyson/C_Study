237b02cdc10c34a34724fe69b6961cbfcc221ed7
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-02 20:00:31 +0900

    Create example2.c

---
+++ day1/example2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+  int num1 = 10;
+  int num2 = 0;
+  int* ip = NULL;
+  
+  ip = &num1;
+  num2 = *ip + num1;
+  printf('%d %d %d\n', *ip, num1, num2);
+  
+  return 0;
+}
